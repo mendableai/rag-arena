6357dcc498f6040fd752c5393d7498e9aa8b344d
leonardogrig <48296347+leonardogrig@users.noreply.github.com>
2024-02-23 01:55:38 -0300

    brief ingestion system for tests

--- app/api/retrievers/dynamic-retriever/tools/variables.ts
+++ app/api/retrievers/dynamic-retriever/tools/variables.ts
@@ -1,8 +1,6 @@
 import { type AttributeInfo } from "langchain/schema/query_constructor";
 
-export const AGENT_SYSTEM_TEMPLATE = `You are a stereotypical robot named Robbie and must answer all questions like a stereotypical robot. Use lots of interjections like "BEEP" and "BOOP".
-
-If you don't know how to answer a question, use the available tools to look up relevant information. You should particularly do this for questions about LangChain.`;
+export const AGENT_SYSTEM_TEMPLATE = `You should only answer based on what you find in the documents. If you don't know how to answer a question just say you can't. Try to be concise and to the point.`;
 
 export const attributeInfo: AttributeInfo[] = [
     {
---
+++ app/ingest/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { useState, type FormEvent } from "react";
+
+export default function UploadDocumentsForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [document, setDocument] = useState("");
+  const ingest = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const response = await fetch("/api/ingest", {
+      method: "POST",
+      body: JSON.stringify({
+        text: document,
+      }),
+    });
+    if (response.status === 200) {
+      setDocument("Uploaded!");
+    } else {
+      const json = await response.json();
+      if (json.error) {
+        setDocument(json.error);
+      }
+    }
+    setIsLoading(false);
+  };
+  return (
+    <form onSubmit={ingest} className="flex max-w-3xl m-auto mt-10 mb-4">
+      <Textarea
+        value={document}
+        onChange={(e) => setDocument(e.target.value)}
+      ></Textarea>
+
+      <Button type="submit" disabled={isLoading}>
+        Upload
+      </Button>
+    </form>
+  );
+}
