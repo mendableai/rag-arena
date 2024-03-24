a5c432c87eeb371385d5125d38038d3f272a47c0
leonardogrig <48296347+leonardogrig@users.noreply.github.com>
2024-03-24 01:46:45 -0300

    dynamic cache disable

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,6 +1,7 @@
 import MainLeaderboard from "@/components/leaderboard";
 import Footer from "@/components/theme/footer";
 import Header from "@/components/theme/header";
+export const dynamic = 'force-dynamic';
 
 export default async function Leaderboard() {
   return (
--- components/leaderboard/index.tsx
+++ components/leaderboard/index.tsx
@@ -1,4 +1,3 @@
-export const dynamic = 'force-dynamic'
 import {
   Table,
   TableBody,
